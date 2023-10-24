3e8310b5791ed592d65f79eb6e3352b290914b06
Gabriel Arango <g.arangoc@uniandes.edu.co>
2023-10-23 23:27:09 -0500

    creacion de los .py de los modelos beta del sistema de vectores

--- run/modelo_SIS.py
+++ run/modelo_SIS.py
@@ -1,1 +1,115 @@
-#this file his only for modeling my model
+# Modelo de Ecuaciones - Gabriel Arango - 23 oct 2023
+from scipy.integrate import odeint
+import numpy as np
+import matplotlib.pyplot as plt
+
+def model(variables_population, t):
+
+    H_s = variables_population[0] 
+    H_i = variables_population[1]
+
+    VS = [variables_population[i] for i in range(2, 2+num_species)]
+    VI = [variables_population[i] for i in range(2+num_species, 2+num_species+num_species)]
+
+    derivadasVS = list()
+    derivadasVI = list()
+
+    for i in range(num_species):
+        interaction_V = 0
+        for j in range(num_species):
+            if i != j:
+                result = gamma[i][j] * (VS[i] + VI[i])  * (VS[i] + VI[i])
+                interaction_V += result
+
+        dtVS = r[i] * (VS[i] + VI[i]) - b[i] * p[i] * VS[i] * H_i - u_s[i]* VS[i] + a * (VS[i] + VI[i])  * H_i - interaction_V
+        dtVI = b[i] * p[i] * VS[i] * H_i - u_i[i] * VI[i]
+
+        derivadasVS.append(dtVS)
+        derivadasVI.append(dtVI)
+
+    infection_H = 0
+
+    for i in range(num_species):
+        infection_H = b[i] * p[i] * VI[i]
+
+    dtH_s = r_h * (H_s + H_i) - infection_H * H_s - g_s * H_s
+    dtH_i = infection_H * H_s - g_i * H_i
+
+    derivadas = list()
+    derivadas.append(dtH_s)
+    derivadas.append(dtH_i)
+    
+    for i in range(len(VS)):
+        derivadas.append(derivadasVS[i])
+
+    for i in range(len(VI)):
+        derivadas.append(derivadasVI[i])
+
+    return tuple(derivadas)
+
+
+#NO SE SI ESTO VA ACA O VA EN OTRA CARPETA :)
+
+variables_population = (1000,100,                       # Variables iniciales de HOST
+                        10000,10000,10000,10000,0,0,    # Variables iniciales de Vectores Suceptible
+                        1000,1000,1000,1000,0,0)        # Variables inicialed de Vectores Infectados
+
+r = [0/1000,0/1000,0/1000,0/1000,0/1000,0/1000]         # Variables de crecimiento de Vectores
+b = [0.0001,0.0001,0.0001,0.0001,0.0001,0.0001]         # Variables de biting rate de Vectores
+p = [0.01,0.01,0.01,0.01,0.01,0.01]                     # Variables de probabilidad de infectar de los Vectores
+u_s = [0/1000,0/1000,0/1000,0/1000,0/1000,0/1000]       # Variables de muerte de Vectores Suceptibles
+u_i = [0/100,0/100,0/100,0/100,0/100,0/100]             # Variables de muerte de Vectores Infectados
+a = 0
+
+
+gamma =np.zeros((6,6))                                  # Matriz de interaccion NULA
+
+'''
+gamma = np.random.random((6,6))/100000000               # Matriz de interaccion 
+'''
+
+r_h = 0/1000        # Variable de crecimiento de Host
+g_s = 0/1000        # Variable de muerte de Host Suceptible
+g_i = 0/1000        # Variable de muerte de HOst Infectado
+
+num_species = 6     # Numero de especies en el modelo
+
+vecTime = np.linspace(0,1000,1000)      # Vector de Tiempo
+
+solve = odeint(model,variables_population, vecTime)
+
+fig, ax = plt.subplots(1,4, figsize = (19,3))
+
+ax[0].plot(solve[:,0], 'b', label = 'Suceptible population')
+ax[0].plot(solve[:,0] + solve[:,1], 'k', label = 'max pupolation')
+ax[1].plot(solve[:,1], 'r',label = 'Infected population')
+
+ax[2].plot(solve[:,2], 'b', label = 'Vector 1')
+ax[2].plot(solve[:,3], 'y', label = 'Vector 2')
+ax[2].plot(solve[:,4], 'r', label = 'Vector 3')
+ax[2].plot(solve[:,5], 'g', label = 'Vector 4')
+ax[2].plot(solve[:,6], 'purple', label = 'Vector 5')
+ax[2].plot(solve[:,7], 'cyan', label = 'Vector 6')
+
+ax[3].plot(solve[:,8], 'b', label = 'Vector 1')
+ax[3].plot(solve[:,9], 'y', label = 'Vector 2')
+ax[3].plot(solve[:,10], 'r', label = 'Vector 3')
+ax[3].plot(solve[:,11], 'g', label = 'Vector 4')
+ax[3].plot(solve[:,12], 'purple', label = 'Vector 5')
+ax[3].plot(solve[:,13], 'cyan', label = 'Vector 6')
+
+ax[0].grid(1)
+ax[1].grid(1)
+ax[2].grid(1)
+ax[3].grid(1)
+
+ax[0].legend(loc = 'best')
+ax[1].legend(loc = 'best')
+ax[2].legend(loc = 'best')
+ax[3].legend(loc = 'best')
+
+ax[0].set_title('HOST')
+ax[1].set_title('HOST')
+ax[2].set_title('Suceptible Vectors')
+ax[3].set_title('Infected Vectors')
+plt.show()
--- run/modelo_takimoto.py
+++ run/modelo_takimoto.py
@@ -1,1 +1,108 @@
-# this file is only for modeling
+# this file is only for modeling - Nicolas
+
+import sympy as sym
+import numpy as np 
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+
+np.random.seed(42)
+
+num_species = 4
+mu_H = 0.01 #Número real
+lambda_H = np.random.uniform(1000/3650, 1000/1500) #Número real
+lambda_V = np.random.uniform(5*10**3/3650, 5*10**3/1500,size=num_species)
+gamma = 0.2 #Número real
+tau_HV = np.random.rand(num_species) #Lista de números reales donde cada posición representa la especie i del vector
+tau_VH = np.random.rand(num_species)
+mu_V = np.random.rand(num_species)
+
+sigma_V = np.random.rand(num_species)
+sigma_H = np.random.rand(num_species)
+
+def b_i(V, H):
+    return (sigma_V[i]*V*sigma_H[i]*H)/(sigma_V[i]*V + sigma_H[i]*H)
+
+def model_HostVectorNspecies(variables, t, mat_a):
+    H_S = variables[0]
+    H_I = variables[1]
+    V_S = [variables[i]  for i in range(2, 2+num_species)]
+    V_I = [variables[i]  for i in range(2+num_species, 2+2*num_species)]
+    
+    derivatives = list()
+    
+    dH_Sdt = lambda_H - mu_H*H_S + gamma*H_I - np.sum([tau_HV[i]*(V_I[i])/(V_S[i]+V_I[i])*(b_i(V_S[i]+V_I[i], H_S+H_I)*H_S)/(H_S+H_I) for i in range(num_species)])
+    dH_Idt = - mu_H*H_I - gamma*H_I + np.sum([tau_HV[i]*(V_I[i])/(V_S[i]+V_I[i])*(b_i(V_S[i]+V_I[i], H_S+H_I)*H_S)/(H_S+H_I) for i in range(num_species)])
+    
+    derivatives = derivatives + [dH_Sdt, dH_Idt]
+
+    for i in range(num_species):
+        dV_Sidt = -mu_V[i]*V_S[i] - tau_VH[i]*(H_I)/(H_S+H_I)*(b_i(V_S[i]+V_I[i], H_S+H_I)*V_S[i])/(V_S[i] + V_I[i]) + lambda_V[i]*(1-np.sum([mat_a[i,j]*(V_S[j]+V_I[j]) for j in range(num_species)]))
+        derivatives.append(dV_Sidt)
+        
+    for i in range(num_species):
+        dV_Iidt = -mu_V[i]*V_I[i] + tau_VH[i]*(H_I)/(H_S+H_I)*(b_i(V_S[i]+V_I[i], H_S+H_I)*V_S[i])/(V_S[i] + V_I[i]) 
+        derivatives.append(dV_Iidt)
+    
+    return derivatives
+
+
+#NO SE SI ESTO VA ACA O EN OTRA CARPETA
+vecTime = np.linspace(0, 500, 100)
+H_total = 100
+p_H_Infected = np.random.uniform(0, 0.9)
+V_total = np.random.uniform(10, 50, size=num_species)
+p_V_Infected = np.random.uniform(0, 0.9, size=num_species)
+
+mat_a_test = np.random.uniform(1/(50), 1/(100), size=(num_species, num_species))
+mat_a_test[0,0] = 0
+mat_a_test[1,1] = 0
+mat_a_test[2,2] = 0
+mat_a_test[3,3] = 0
+
+condInit = [H_total*(1-p_H_Infected), H_total*(p_H_Infected)]
+
+for i in range(num_species):
+    condInit.append(V_total[i]*(1-p_V_Infected[i]))
+
+for i in range(num_species):
+    condInit.append(V_total[i]*(p_V_Infected[i]))
+    
+simu = odeint(model_HostVectorNspecies, condInit, vecTime, args=(mat_a_test, ))
+
+
+fig, ax = plt.subplots(2, 3, figsize=(14,8), sharex=True)
+
+ax[0,0].plot(vecTime, simu[:,0], label='Host Susceptible')
+ax[0,0].plot(vecTime, simu[:,1], label='Host Infected')
+ax[0,0].grid()
+ax[0,0].set_xlabel('Time')
+ax[0,0].set_ylabel('Population')
+ax[0,0].legend(loc='best')
+
+ax[0,1].plot(vecTime, simu[:,2], label='Vector 1 Susceptible')
+ax[0,1].plot(vecTime, simu[:,6], label='Vector 1 Infected')
+ax[0,1].grid()
+ax[0,1].set_xlabel('Time')
+ax[0,1].set_ylabel('Population')
+ax[0,1].legend(loc='best')
+
+ax[0,2].plot(vecTime, simu[:,3], label='Vector 2 Susceptible')
+ax[0,2].plot(vecTime, simu[:,7], label='Vector 2 Infected')
+ax[0,2].grid()
+ax[0,2].set_xlabel('Time')
+ax[0,2].set_ylabel('Population')
+ax[0,2].legend(loc='best')
+
+ax[1,0].plot(vecTime, simu[:,4], label='Vector 3 Susceptible')
+ax[1,0].plot(vecTime, simu[:,8], label='Vector 3 Infected')
+ax[1,0].grid()
+ax[1,0].set_xlabel('Time')
+ax[1,0].set_ylabel('Population')
+ax[1,0].legend(loc='best')
+
+ax[1,1].plot(vecTime, simu[:,5], label='Vector 4 Susceptible')
+ax[1,1].plot(vecTime, simu[:,9], label='Vector 4 Infected')
+ax[1,1].grid()
+ax[1,1].set_xlabel('Time')
+ax[1,1].set_ylabel('Time')
+ax[1,1].legend(loc='best')
